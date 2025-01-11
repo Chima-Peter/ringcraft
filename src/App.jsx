af2ce9b5416004daf8938c20c052799d254fe230
Mobi <93464955+Chima-Peter@users.noreply.github.com>
2025-01-11 18:25:40 -0500

    set up lighting for the ring model

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,16 @@ const App = () => {
   const { outerColor, setOuterColor, innerColor, setInnerColor } = useColorContext()
 
   return (
-    <main className="flex justify-between">
+    <main className="flex justify-between bg-[#f3f4f6] w-[100%] p-6">
       <Canvas style={{ height: '100vh', width: '70vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         Hello ThreeJS
-        <OrbitControls enableDamping enablePan enableZoom enableRotate />
-        <ambientLight intensity={0.5} />
-        <directionalLight castShadow={false} position={[ 5, 5, 5 ]} intensity={10} color={0xffffff} />
-        <color attach="background" args={['#D3D3D3']} />
+        <OrbitControls enableDamping enablePan enableZoom />
+        <directionalLight castShadow={true} position={[2, 5, 3]} intensity={8} />
+        <color attach="background" args={['#f3f4f6']} />
         <Suspense fallback={<div>Loading...</div>}></Suspense>
         <Model />
       </Canvas>
-      <section className="h-[100vh] my-4 mx-6">
+      <section className="h-[90vh] overflow-y-auto w-[500px] shadow-xl rounded-2xl bg-white text-black flex gap-4 py-4 px-6 flex-col ">
         <h1>
           Customise your ring
         </h1>
@@ -49,6 +48,9 @@ const App = () => {
             Dont't like ring color? Change it
           </p>
         </div>
+        <h3 className="text-5xl text-red-600">
+          hello
+        </h3>
       </section>
     </main>
   )
